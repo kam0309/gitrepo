a5eaeed1edaece7a25054b6784a87b104e7c14c0
kl2ag1 <kamil.warzycki@o2.pl>
2017-09-29 08:46:43 +0200

    trójkat

--- cpp/trojkat.cpp
+++ cpp/trojkat.cpp
@@ -4,6 +4,7 @@
 
 
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -11,11 +12,29 @@ int main(int argc, char **argv)
 {
     
     int a,b,c;
+    int obwod = 0;
+    float p ;
+    float pole ;
     a = b = c =0;
-    
-   cout<<"Podaj bok: " ;
+    cout<<"Podaj bok: " ;
    cin >> a >> b >> c;
-   cout << "Obwód: " << a + b + c << endl;
+   //~if (a + b > c ){ 
+       //~if (a + c > b) { 
+       //~if(b + c > a){
+           //~cout << "Da się utworzyć";
+           //~}    
+           //~}
+       //~}
+    if (a + b > c && a + c > b && b + c > a) {
+        obwod = a + b + c;
+        cout << "Obwód: " << obwod << endl;
+        p = 0.5 * obwod;
+        cout << "współczynnik: " << p << endl;
+        pole = sqrt(p*(p - a)*(p-b)*(p - c));
+        cout << "Pole ma" << pole;
+    } else {
+        cout << "Nie da się" ;
+    }
      
    
 	return 0;
