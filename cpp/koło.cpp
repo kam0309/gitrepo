62694f846c87411e7d4234bb3df6eb316f377304
kam0903 <kamil.warzycki@o2.pl>
2017-10-02 20:31:57 +0200

    Kolo

---
+++ cpp/koło.cpp
@@ -0,0 +1,24 @@
+/*
+ * hello.cpp
+ */
+
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    
+    int r=0;
+    
+    
+    cout << "Podaj promien:" << endl;
+    cin >> r;
+    cout << "Pole kola = " <<M_PI*r*r << endl;
+    cout << "Obwod =" <<M_PI*r*2 << endl;
+   
+	return 0;
+}
+
