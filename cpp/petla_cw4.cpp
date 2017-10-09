d61d1d1efe84ca4a8c0139eba41d1b01b274be18
kam0903 <kamil.warzycki@o2.pl>
2017-10-09 21:45:30 +0200

    cwiczenia

---
+++ cpp/petla_cw4.cpp
@@ -0,0 +1,25 @@
+/*
+ * petla_cw4.cpp
+ */
+
+
+#include <iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int a;
+for(a=10;a<100;a++)
+{
+if(a%6==0)
+{
+cout<<a<<endl;
+}
+}
+getch();
+	
+	return 0;
+}
+
