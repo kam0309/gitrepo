0d961e6040a053a4afdc384d25298083c868906a
kl2ag1 <kamil.warzycki@o2.pl>
2017-10-24 10:37:23 +0200

    tabele2

--- cpp/tabele.cpp
+++ cpp/tabele.cpp
@@ -7,6 +7,20 @@
 
 using namespace std;
 
+
+void ile5(int tab[], int ile){
+    int i;
+    int licznik =0;
+    for (i=0; i < ile; i ++){
+        if (tab[i] % 5 == 0 )
+        licznik5++;
+        
+        
+        }
+    cout << "Podzielnych przez 5: " << licznik5 << endl;
+    
+    }
+
 void pobierzliczby(int tab[], int ile) {
     int i= 0;
     for (i = 0; i < ile; i ++) {
@@ -23,13 +37,18 @@ void pobierzliczby(int tab[], int ile) {
     cout << "suma liczb: "<< suma << endl;        
 }
 
-void najminiejsza (int tab [], int ile){
-    int = 0;
-    int suma =0;
+void najmniejsza (int tab [], int ile){
+    int mini = tab[0];
+    for (int i = 1; i < mini; i ++) {
+        if (mini > tab[i])
+        mini= tab[i];
+    }
+    cout << "najmiejsza liczba: " << mini << endl;
+}
     //funkcja znajduje i drukuje njmniejsza liczbe z tabeli
     
     
-    }
+
 int main(int argc, char **argv)
 {
     int rozmiar = 0;
@@ -40,7 +59,8 @@ int main(int argc, char **argv)
     
     pobierzliczby(liczby, rozmiar);
     sumuj (liczby , rozmiar);
-        
+    najmniejsza(liczby , rozmiar);
+    ile5 (liczby , rozmiar);
    
 	return 0;
 }
