0305d7231f15f65963f9b3533d321de87b213608
kam0309 <kamil.warzycki@o2.pl>
2018-01-08 12:36:38 +0100

    sortowanie

---
+++ python/sortowanie.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#  sortowanie.py
+ 
+ 
+def sort_wstaw(lista):
+     """wersja liniowa"""
+    for i in range(1, len(lista)):
+        el = lista[i]
+        k = i - 1
+        while k >= 0 and lista[k] > el:
+            lista[k + 1] = lista[k]
+            k = k - 1
+        lista[k + 1] = el 
+    return lista
+
+def sort_wstaw_bin(lista):
+    for i in range(1, len(lista)):
+        el = lista[i]
+        k = szukaj_bin(0, i, lista, el) #wyszukiwanie binarne miejsca do wstawienia el 
+        pass
+
+
+def main(args):
+    lista = [4, 3, 7, 0, 2, 3, 1, 9]
+    print(lista)
+    # [3, 4, 7, 0, 2, 3, 1, 9]
+    # [3, 4, 7, 0, 2, 3, 1, 9]
+    # [0, 3, 4, 7, 2, 3, 1, 9]
+    print(sort_wstaw(lista))
+    return 0
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
