a825d42f447957e726cab01375020d35f10f4cbf
kam0309 <kamil.warzycki@o2.pl>
2018-01-16 10:35:58 +0100

    parzyste

---
+++ cpp/parzyste.cpp
@@ -0,0 +1,19 @@
+/*
+ * parzyste.cpp
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int a;
+    cout <<"Podaj wartośc a:" <<endl;
+    cin >> a;
+    if 0 < a 
+	
+	return 0;
+}
+
