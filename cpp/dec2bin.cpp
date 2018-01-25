201a3d4286fd593702ff816e2029824d52679787
kam0309 <kamil.warzycki@o2.pl>
2018-01-25 08:46:22 +0100

    dec2bin

---
+++ cpp/dec2bin.cpp
@@ -0,0 +1,22 @@
+/*
+ * dec2bin.cpp
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int liczba = 120;
+    do{
+        ;
+        }while();
+    
+    }
+{
+	
+	return 0;
+}
+
