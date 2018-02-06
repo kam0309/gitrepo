f462749bf54b5babbc896ad064f46f1e765e203c
kam0309 <kamil.warzycki@o2.pl>
2018-02-06 10:35:23 +0100

    cezar

---
+++ python/szyfr_Cezara.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  szyfr_Cezara.py
+
+
+def szyfruj_cezar(tekst, klucz):
+    klucz = klucz % 26
+    szyfrogram = ""
+    for znak in tekst:
+        znak = znak.upper()
+        ascii = ord(znak) + klucz
+        if ascii > 90:
+            ascii -= 26
+        szyfrogram += chr(ascii)
+    return szyfrogram
+    
+def deszyfruj(szyfrogram, klucz):
+    pass
+
+# obsłużyć spacje 
+#obsłużyć małe i duże litery
+
+def main(args):
+    tekst = input("Podaj tekst: ")
+    klucz = int(input("Klucz: "))
+    
+    szyfrogram = szyfruj_cezar(tekst, klucz)
+    print(szyfrogram)
+    print(deszyfruj(szyfrogram, klucz))
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
